e47a15fafa892cde0ba45eadbb0f8053717b6983
Andy Wolff <andywolff@wpi.edu>
2013-12-08 18:47:12 -0500

    scripts for robustness study

---
+++ scripts/begin_robustness_study_string.sh
@@ -0,0 +1,2 @@
+rosrun image_view image_view image:=/usb_cam/image_raw _filename_format:="Pictures/RobustnessStudy/String/%04i.jpg" _window_name:=RobustnessStudyVideoStream
+
---
+++ scripts/rclick_on_image.sh
@@ -0,0 +1,21 @@
+#store the currently-active window
+export old_window=$(xdotool getactivewindow)
+#get all windows that match the name
+export my_window=($(xdotool search --name RobustnessStudyVideoStream | sed 's/:.*//'))
+#get the last matching window
+export my_window=${my_window[${#my_window[@]} - 1]}
+#get the mouse position
+export stuff=$(xdotool getmouselocation --shell)
+#put it into $X and $Y
+eval $stuff
+#activate the desired window
+xdotool windowactivate $my_window
+#move the mouse over it
+xdotool mousemove --window $my_window 1 1
+#right click
+xdotool click --window $my_window 3
+#move the mouse back to its original position
+xdotool mousemove $X $Y
+#reactivate the originally active window
+xdotool windowactivate $old_window
+
